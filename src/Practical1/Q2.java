904ce0bd6186c1a00146d07d3f51863ad23fb3b0
arouven <arouven@arouven-Lenovo-ideapad-330-15IKB>
2019-08-16 14:57:47 +0400

    lab1

---
+++ src/Practical1/Q2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practical1;
+
+/**
+Qu.2 “MauriJobs” is a new company located at Ebene Business Park. It allows job
+applicants to view jobs, search for a specific job, apply for the job online, and
+monitor the status of the application which may be accepted for an interview or
+rejected. The applicant must be registered with the company before making any
+application and once a member is called for an interview he is charged a nominal
+fee of Rs 150. If an applicant is not selected for any interview for a period of 3
+months, his account is blocked and he is requested to upgrade his qualifications.
+Perform the following operations:-
+(i) create an applicant
+(ii) create a job
+(iii) create an application for a job
+(iv) accept the application
+(v) request the applicant to pay his charge to “Online Jobs”
+(vi)display appropriate messages to the user
+ */
+public class Q2 {
+
+    public static void main(String[] args) {
+        Applicant a1 = new Applicant();
+        Job j1 = new Job();
+        
+    }
+    
+}
+class Applicant{
+    
+    //function
+    //view jobs
+    //search jobs
+    //apply for job
+    //monitor--accept reject
+}
+class Job{
+    
+    //function
+    //view jobs
+    //search jobs
+    //apply for job
+    //monitor--accept reject
+}
